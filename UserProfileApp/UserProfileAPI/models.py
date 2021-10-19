302962e06ea7509e76230bcc4307acce2470508d
Krupesh Savaliya <krupesh.savaliya@kevit.io>
2021-10-19 17:49:51 +0530

    Create User model

--- UserProfileApp/UserProfileAPI/models.py
+++ UserProfileApp/UserProfileAPI/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=60, blank=False)
+    email = models.CharField(max_length=60, blank=False, unique=True)
+    phone_no = models.FloatField(max_length=10, blank=False, unique=True)
+    password = models.CharField(max_length=100, blank=False)
+    gender = models.CharField(max_length=6, default='Male')
+
+    def __str__(self):
+        return self.name
